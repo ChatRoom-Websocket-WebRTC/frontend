94a7eaabe0bc1d0690f5a14efa2c650dd7798bca
Emadmousavi <emadmoosavi79@gmail.com>
2023-01-20 20:12:11 +0330

    add basic chatroom without sending option

--- src/routes/AppRouter.jsx
+++ src/routes/AppRouter.jsx
@@ -2,13 +2,16 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import NotFoundPage from "../components/NotFoundPage";
 import Header from "../components/Header";
 import Footer from "../components/Footer";
+import Chat from "../components/Chat";
+import Conversation from "../components/Chat/Conversation";
 
 const AppRouter = () => {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/header" element={<Header />} />
-        <Route path="footer" element={<Footer />} />
+        <Route path="/footer" element={<Footer />} />
+        <Route path="/" element={<Conversation />} />
         <Route path="*" element={<NotFoundPage />} />
       </Routes>
     </BrowserRouter>
